39101a52787cc220c9baa6ab265436931910756f
Kibeom Kim <terr1976@hotmail.com>
2016-05-15 13:47:59 -0400

    Complete tenant and users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,14 +3,14 @@ class HomeController < ApplicationController
 
   def index
     if current_user
-      if session[:tennant_id]
-        Tenant.set_current_tenant session[:tennant_id]
+      if session[:tenant_id]
+        Tenant.set_current_tenant session[:tenant_id]
       else
         Tenant.set_current_tenant current_user.tenants.first
       end
 
       @tenant = Tenant.current_tenant
-      params[:tennant_id] @tenant.id
+      params[:tenant_id] = @tenant.id
     end
   end
 end
